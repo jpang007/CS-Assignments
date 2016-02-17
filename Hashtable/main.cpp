767af871b1158c639af0dfd938b3e02e09c22234
Jeremy Pang <jpang007@ucr.edu>
2016-02-17 15:04:23 -0800

    Update main.cpp

--- Hashtable/main.cpp
+++ Hashtable/main.cpp
@@ -1,216 +1,222 @@
-#include <string>
-#include <iostream>
 #include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <iostream>
 #include <iterator>
 #include <fstream>
 #include <sstream>
-#include "assn4.h"
-#include <list>
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
+#include <ctype.h>
+#include "QuadraticProbing.h"
+#include "SeparateChaining.h"
 
 using namespace std;
 
+// Actual driver ready to do work
+int collisioncounter;
+
+string toLowerCase(string s) // function to help convert words to all lower case
+{
+    string r = s;
+    for (int i = 0; i < r.size(); i++)
+        if (r[i] >= 'A' && r[i] <= 'Z')
+            r[i] = tolower(r[i]);
+    return r;
+}
+
+void removePunct(string & str)
+{
+	str.erase(remove_if(str.begin(), str.end(), static_cast<int(*)(int)>(&ispunct)),str.end());
+}
 
 int main(int argc, char *argv[]) {
-
-    vector <string> inputfiles;
-    string inputtedfilename, firstoutput, relationshipseperator;
-    string line1, line2;
-    ofstream output;
-    list<Fraction> ab, originalforumula;
-        
-    ifstream the_input(argv[1]); // Opens the file and pushes back everything into a string
+    vector <string> inputfiles; // vector of string to hold whats inside the command line
+    vector <string> Dictionarywords; // vector of the dictionary words to compare to
+    vector <string> takeinfiles; // vector to hold string *toupper & *tolower the
+    string inputtedfilename, inputtedfilename2, inputtedfilename3, firstoutput, secondoutput;
+    // 1) first command line argument 2) second one 3) third one 4) string to output to .out 5) string to 
+    // output to .stats
+    string outputfilename; // string used to attached suffix to output files .stats and .out
+    string inputline; // string for inputting text into vector 
+    string line1, line2; // the strings used to help dechiper the command line
+    vector <int> chaincounter; // holds all the length of list for chaining 
+    
+    ifstream the_input(argv[3]); // Opens the file and pushes back everything into a string
     if (!the_input.is_open() ) {
         cout<<"Could not open file\n";
     }
     else {
     string x;
-    while (the_input >> x )
+    while (the_input >> x ) {
+        removePunct(x);
         inputfiles.push_back(x);
     }
-        
-    //for (int i = 0; i < inputfiles.size(); i++) { // just to check whats inside
-    //    cout << inputfiles.at(i) << " ";
-    //}
-    
-    string yolo;  // Beings to seperate the input.txt into parts line by line 
-    yolo = argv[1];
-    
-    ifstream inputgobblegobble;
-    inputgobblegobble.open(yolo.c_str());
-    
-    if (inputgobblegobble.is_open()) {
-        inputtedfilename = argv[1]; // Attaches the correct name and suffix to output files
-                
-        inputtedfilename = inputtedfilename.substr(0, inputtedfilename.size() - 4);
-                
-        firstoutput = inputtedfilename + ".out";
-            
-        output.open(firstoutput.c_str());
-        while (inputgobblegobble.good()) {
-            vector <int> a;
-            vector <int> outputvector;
-            getline(inputgobblegobble, line1);
-            getline(inputgobblegobble, line2);
-            
-            cout << line1 << endl;
-            cout << line2 << endl;
-            
-            if (line1 == "f") { // if fraction
-                if stream(line2);
-                int temp = 0;
-                while(stream >> temp) {
-                    a.push_back(temp);
-                }
-                
-                for (int i = 0; i < a.size() - 1; i = i + 2) {
-                        ab.push_back(Fraction(a[i], a[i + 1])); // list to be altered
-                        originalforumula.push_back(Fraction(a[i], a[i + 1])); // original list to be entered into tree
-                } 
-                
-                cout << endl; 
-                
-                
-                list<Fraction>::iterator spot1;
-                spot1 = ab.begin();
-                spot1++;
-                while (spot1 != ab.end()) {
-                    if (*spot1 == ab.front()) {
-                        spot1 = ab.erase(spot1);
-                    }
-                    else {
-                        spot1++;
-                    }
-                } // loop goes thro AB and deletes anything that matches the first value
-                
-                cout << "New list is: ";
-                
-                for (list<Fraction>::iterator it=ab.begin(); it != ab.end(); ++it) {
-                cout << ' ' << *it;
-                }
-                
-                cout << endl;
-                
-                list<Fraction>::iterator itr;
 
-                output << line1 << endl;
-	            for(itr= ab.begin(); itr != ab.end();++itr)
-	            {
-	                output<< *itr << " ";
-	            }
-	            output << endl; // for loop prints out the new edited list
-
-                cout << endl;
-                
-
-                obtree fractiontree;
-                list<Fraction>::iterator obtreeitr;
-                obtreeitr = originalforumula.begin();
-                while(obtreeitr != originalforumula.end()) {//error here 
-                    fractiontree.insertf(obtreeitr); // doesnt work because it only takes in int
-                    obtreeitr++;
-                }
-                cout << "Values in tree are in order: ";
-                fractiontree.print_inorderf();
-                cout << endl;
-                cout << "Values in tree are postorder: ";
-                fractiontree.print_postorderf();
-                
-                originalforumula.clear();
-                
-                fractiontree.sortf(originalforumula);
-                cout << endl;
-                cout << "Ordered list: ";
-                for (list<Fraction>::iterator it=originalforumula.begin(); it != originalforumula.end(); ++it) {
-                cout << ' ' << *it;
-                }
-                        // TODO:: Figure out a way to either input a int or take in a Fraction
-                originalforumula.clear();
-                ab.clear();
-                    
-                cout << endl << endl;
-            }
-            
-            else if (line1 == "i") { // if list is int
-                vector <int> a, originala;
-                stringstream stream(line2);
-                int temp = 0;
-                while(stream >> temp) {
-                    a.push_back(temp);
-                    originala.push_back(temp);
-                }
-                
-                int x = a[0];
-                for (int i = 1; i < a.size(); i++) { // input values into a vector<int> then erase the similar ones
-                    if (x == a[i]) {
-                        a.erase(a.begin() + i);
-                    }
-                }
-                
-                for (int i = 0; i < originala.size(); i = i + 1) {
-                        ab.push_back(Fraction(originala[i])); // list to be altered
-                        originalforumula.push_back(Fraction(originala[i])); // original list to be entered into tree
-                } 
-                cout << "New list is: ";
-                
-                for (list<Fraction>::iterator it=ab.begin(); it != ab.end(); ++it) {
-                cout << ' ' << *it;
-                }
-                
-                list<Fraction>::iterator itr;
-            
-                output << line1 << endl;
-	            for(itr= ab.begin(); itr != ab.end();++itr)
-	            {
-	                output<< *itr << " ";
-	            }
-	            output << endl; 
-                obtree inttree; // make a binary tree of ints
-                
-                cout << endl;
-                int n = 1, i = 0;
-                while (n == 1) {
-                    inttree.insert(originala[i]);
-                    i++;
-                    if (i == originala.size()) {
-                        n = 0;
-                    }
-                }
-                
-                cout << endl;
-                cout << "Values in the tree are postorder: ";
-                
-                inttree.print_postorder();
-                
-                cout << endl;
-                cout << "Values in the tree are in order: ";
-                inttree.print_inorder();
-                cout << endl;
-                
- 
-                
-                inttree.sortf(originalforumula);
-                
-                for (list<Fraction>::iterator it=originalforumula.begin(); it != originalforumula.end(); ++it) {
-                cout << ' ' << *it;
-                }
-                
-                ab.clear();
-                originalforumula.clear();
-
-                cout << endl;
-            }
-            
-            
-        }
-            
-        cout << endl;
     }
     
+    cout << "All words in inputted file lowercase are: " << endl;
+    for (int i = 0; i < inputfiles.size(); i++) { // converts all the elements in vector<string> inputfiles
+    // to lowercase and prints them out to check
+        inputfiles[i] = toLowerCase(inputfiles[i]);
+        cout << inputfiles[i] << " ";
+    }
+    cout << endl;
     
+    /*
+    for (int i = 0; i < inputfiles.size(); i++) { // just to check whats inside
+        cout << inputfiles.at(i) << " ";
+    }
+    */
     
-    return 0;     
-}
+    inputtedfilename = argv[1]; // looks at the first command line to see which command to do
+    inputtedfilename2 = argv[2]; // looks at the second command line to see which command to do
+    inputtedfilename3 = argv[3]; // Attaches the correct name and suffix to output files
     
+    ifstream inputgobblegobble; // ifstream caled inputgobblegobble
+    inputgobblegobble.open(inputtedfilename3.c_str()); // inputgobblegobble opens inputted
+    //filename 3 aka input.txt
+    ofstream output, output2; // creates two ofstream output, output for .stats and .out
+    ifstream dictionaryinput; // holds the .txt files of the two dictionary libraries later
+    
+    int periodfound = inputtedfilename3.find('.');
+    inputtedfilename3 = inputtedfilename3.substr(0,periodfound); // Translates inputtedfilename3 
+    // substrings at the . so it can accept name suffix input file
+    
+    firstoutput = inputtedfilename3 + ".out";
+    secondoutput = inputtedfilename3 + ".stats";
+    
+    if (inputgobblegobble.is_open()) {
+        if (inputtedfilename == "-1") {
+            cout << "Selected Smalldic.txt " << endl;
+            dictionaryinput.open("Smalldic.txt");
+            while (getline(dictionaryinput, inputline)) {
+                Dictionarywords.push_back(inputline); // Uses the Smalldic.txt and
+                // push_backs all the words into a vector<string> 
+            }
+            cout << "Total words stored in string Dictionarywords: " << Dictionarywords.size() << endl;
+        }
         
+        if (inputtedfilename == "-2") {
+            cout << "Selected Ispell.txt " << endl;
+            dictionaryinput.open("Ispell.txt");
+            while(getline(dictionaryinput, inputline)) {
+                Dictionarywords.push_back(inputline); //Uses the Ispell.txt and 
+                //push_backs all the words into a vector<string>
+            }
+            cout << "Total words stored in string Dictionarywords: " << Dictionarywords.size() << endl;
+        }
+        
+        if (inputtedfilename2 == "-chain") { // If the user specififes using a chain
+            HashTableS<int> h1;
+            
+            cout << ">>Selected Chain ";
+            int i, spot = 0;
+            
+            for (i = 0; i != Dictionarywords.size(); i++) {
+                h1.insertS(hashS(Dictionarywords[i])); // makes a hashtable of the dictionary words
+            }
+            
+            for (i = 0; i != Dictionarywords.size(); i++) { // If no ERROR then all files are present
+                if (h1.containsS(hashS(Dictionarywords[i]) ) == false) {
+                    cout << "ERROR" << endl;
+                }
+            }
+            
+            output.open(firstoutput.c_str());
+                for (i = 0; i != inputfiles.size(); i++) {
+                    if (h1.containsS(hashS(inputfiles[i])) == false) { // if the dictionary hash table
+                    // does not contain the mispelled words throw those into a output.txt
+                        output << inputfiles[i] << endl;
+                    }
+                }
+            output.close();
+            
+            cout << endl;
+            output2.open(secondoutput.c_str());
+            output2 << "Length of tables | " << "Table entries that appeared" << endl;
+            for (int i = 0; i < inputfiles.size(); i++ ) {
+                int holdera = h1.mahchain(hashS(inputfiles[i]));
+                cout << holdera;
+                chaincounter.push_back(holdera);
+            }
+            
+            sort(chaincounter.begin(), chaincounter.end());
+        
+            for (int i = 0; i < chaincounter.size(); i++) { // double for loop to output the chaincounter
+                int countercounter = 0;
+                if (chaincounter[i] != chaincounter[i + 1]) {
+                    output2 << chaincounter[i] << "                    ";
+                    if (i == 0) {
+                        countercounter++;
+                    }
+                    for (int z = 0; z < chaincounter.size() - 1; z++) {
+                        if (chaincounter[i] == chaincounter[z + 1]) {
+                            countercounter++;
+                        }
+                    }
+                    output2 << countercounter;
+                    output2 << endl;
+                    }
+            }
+            output2.close();
+            
+        }
+        
+        else if (inputtedfilename2 == "-probe") { // If the user specifies using a probe
+            HashTable<int> a1;
+            
+            cout << ">>Selected Probe" << endl;
+            
+                int i, spot = 0;
+                vector<int> collisioncounterholder;
+                for( i = 0; i != Dictionarywords.size(); i++) {
+                    a1.insert(hash(Dictionarywords[i]));
+          
+                    collisioncounterholder.push_back(collisioncounter);
+                }
+                
+                for (i = 0; i != Dictionarywords.size(); i++) { // If no ERROR then all files are present
+                    if (a1.contains(hash(Dictionarywords[i]) ) == false) {
+                        cout << "ERROR" << endl;
+                    }
+                }
+                
+                output.open(firstoutput.c_str());
+                for (i = 0; i != inputfiles.size(); i++) {
+                    if (a1.contains(hash(inputfiles[i])) == false) {
+                        output << inputfiles[i] << endl;
+                    }
+                }
+                output.close();
+                
+                output2.open(secondoutput.c_str());
+                output2 << "Number of collisions | " << "Number of dictionary words experiencing collsion" << endl;
+
+                
+                sort(collisioncounterholder.begin(), collisioncounterholder.end());
+                
+                for (int i = 0; i < collisioncounterholder.size(); i++) { // double for loop to output the
+                // collisioncounter
+                int countercounter = 0;
+                if (collisioncounterholder[i] != collisioncounterholder[i + 1]) {
+                    output2 << collisioncounterholder[i] << "                    ";
+                    if (i == 0) {
+                        countercounter++;
+                    }
+                    for (int z = 0; z < collisioncounterholder.size() - 1; z++) {
+                        if (collisioncounterholder[i] == collisioncounterholder[z + 1]) {
+                            countercounter++;
+                        }
+                    }
+                    output2 << countercounter;
+                    output2 << endl;
+                    }
+            }
+            output2.close();
+        }
+
+    }
+    
+    cout << endl;
+    return 0;
+}
